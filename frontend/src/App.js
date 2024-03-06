30a48cc29bf073fd9297f9ba15bc234165f52969
HarithRajapaksha <priyaharshanaharith@gmail.com>
2024-03-06 11:26:07 +0530

    login and signUp success

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import logo from './logo.svg';
-import './App.css';
 
+
+import Registraition from './Components/Registraition';
 import AdminLogin from './Components/AdminsLogin'
-//import Registration from './Components/Registraition';
+
 import Signup from './pages/Signup';
 import  Login from './pages/Login';
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import React from "react";
-
 function App() {
   return (
-   <Registraition/>
+
+ <Router>  
+
+   <Routes>
+    <Route exact path="/" element={<AdminLogin/>}></Route>
+    <Route path="/register" element={<Registration/>}></Route> 
+   </Routes>
+
+ </Router>   
   );
 }
 
