c2d1a7456263ed90d8fc38491f85af10bac2aa76
Shehan <shehanjr99@gmail.com>
2024-03-18 23:56:39 +0530

    Layout created for admin login

--- frontend/src/Components/AdminsLogin.js
+++ frontend/src/Components/AdminsLogin.js
@@ -21,149 +21,178 @@ import Alert from '@mui/material/Alert';
 import CheckIcon from '@mui/icons-material/Check';
 
 function Copyright(props) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-          {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-function Alerttheme(){
-    return (
-        
+function Alerttheme() {
+  return (
+
     <Stack sx={{ width: '100%' }} spacing={2}>
-     <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-      This success Alert has a custom icon.
-    </Alert>
+      <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+        This success Alert has a custom icon.
+      </Alert>
     </Stack>
 
-    );
-  }
+  );
+}
 
 const defaultTheme = createTheme();
 
 function AdminsLogin() {
 
-const[email,setEmail]=useState('')
-const[Pass,setPassword]=useState('')
-const[loginmsg,setloginMsg]=useState([])
+  const [email, setEmail] = useState('')
+  const [Pass, setPassword] = useState('')
+  const [loginmsg, setloginMsg] = useState([])
 
-const UserdataSubmit=async(e)=>{
+  const UserdataSubmit = async (e) => {
     e.preventDefault();
 
-   const sendData={email:email,password:Pass}
+    const sendData = { email: email, password: Pass }
 
     try {
-        const alldata=await axios.post("http://localhost:3005/loginAndSign/signIn",sendData)
-        console.log(alldata.data);
-        alert(alldata.data.message);
-        setloginMsg(alldata)
+      const alldata = await axios.post("http://localhost:3005/loginAndSign/signIn", sendData)
+      console.log(alldata.data);
+      alert(alldata.data.message);
+      setloginMsg(alldata)
 
-        if(alldata.data.message==="Login successful"){
-          window.location.href =`/adminHome/${alldata.data.UserId}`;
-        }
-    
+      if (alldata.data.message === "Login successful") {
+        window.location.href = `/adminHome/${alldata.data.UserId}`;
+      }
+
     } catch (error) {
-        console.log("data not send"+error.message)
-        alert(loginmsg.data.message);
+      console.log("data not send" + error.message)
+      alert(loginmsg.data.message);
     }
-}
+  }
 
   return (
-    
-    <ThemeProvider theme={defaultTheme}>
-    <Grid container component="main" sx={{ height: '100vh' }}>
-      <CssBaseline />
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={7}
-        sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-          backgroundRepeat: 'no-repeat',
-          backgroundColor: (t) =>
-            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" noValidate  sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={(e)=>{setEmail(e.target.value)}}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(e)=>{setPassword(e.target.value)}}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={UserdataSubmit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ mt: 5 }} />
-          </Box>
-        </Box>
-      </Grid>
-    </Grid>
-  </ThemeProvider>
-   
+
+    //   <ThemeProvider theme={defaultTheme}>
+    //   <Grid container component="main" sx={{ height: '100vh' }}>
+    //     <CssBaseline />
+    //     <Grid
+    //       item
+    //       xs={false}
+    //       sm={4}
+    //       md={7}
+    //       sx={{
+    //         backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+    //         backgroundRepeat: 'no-repeat',
+    //         backgroundColor: (t) =>
+    //           t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+    //         backgroundSize: 'cover',
+    //         backgroundPosition: 'center',
+    //       }}
+    //     />
+    //     <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+    //       <Box
+    //         sx={{
+    //           my: 8,
+    //           mx: 4,
+    //           display: 'flex',
+    //           flexDirection: 'column',
+    //           alignItems: 'center',
+    //         }}
+    //       >
+    //         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+    //           <LockOutlinedIcon />
+    //         </Avatar>
+    //         <Typography component="h1" variant="h5">
+    //           Sign in
+    //         </Typography>
+    //         <Box component="form" noValidate  sx={{ mt: 1 }}>
+    //           <TextField
+    //             margin="normal"
+    //             required
+    //             fullWidth
+    //             id="email"
+    //             label="Email Address"
+    //             name="email"
+    //             autoComplete="email"
+    //             autoFocus
+    //             onChange={(e)=>{setEmail(e.target.value)}}
+    //           />
+    //           <TextField
+    //             margin="normal"
+    //             required
+    //             fullWidth
+    //             name="password"
+    //             label="Password"
+    //             type="password"
+    //             id="password"
+    //             autoComplete="current-password"
+    //             onChange={(e)=>{setPassword(e.target.value)}}
+    //           />
+    //           <FormControlLabel
+    //             control={<Checkbox value="remember" color="primary" />}
+    //             label="Remember me"
+    //           />
+    //           <Button
+    //             type="submit"
+    //             fullWidth
+    //             variant="contained"
+    //             sx={{ mt: 3, mb: 2 }}
+    //             onClick={UserdataSubmit}
+    //           >
+    //             Sign In
+    //           </Button>
+    //           <Grid container>
+    //             <Grid item xs>
+    //               <Link href="#" variant="body2">
+    //                 Forgot password?
+    //               </Link>
+    //             </Grid>
+    //             <Grid item>
+    //               <Link href="#" variant="body2">
+    //                 {"Don't have an account? Sign Up"}
+    //               </Link>
+    //             </Grid>
+    //           </Grid>
+    //           <Copyright sx={{ mt: 5 }} />
+    //         </Box>
+    //       </Box>
+    //     </Grid>
+    //   </Grid>
+    // </ThemeProvider>
+    <Box width="100vw" height="100vh">
+
+      <Box
+        display="flex"
+        flexDirection="column"
+      >
+        <Avatar>
+          I
+        </Avatar>
+        <Typography>
+
+        </Typography>
+
+        <TextField />
+
+        <TextField />
+
+        <Grid container>
+          <Grid item></Grid>
+          <Grid item></Grid>
+
+        </Grid>
+
+        <Button>
+
+        </Button>
+      </Box>
+
+    </Box>
+
   )
 }
 
